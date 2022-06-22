31e3a25f5274375386998b673e6440b3ef9ba1d6
wojtas_f <wojtas_f@outlook.com>
2022-06-22 18:16:12 +0200

    feat: add login and home view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Views/Login";
+import Home from "./Components/Views/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/login" element={<Login />} exact />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Views/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+import NavBar from "../Partials/NavBar";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
